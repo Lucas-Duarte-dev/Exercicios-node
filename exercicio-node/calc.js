a9f3a6020ddde1cb8d1f79fc5d0c6cdceb210d27
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-04-10 16:47:53 -0300

    Resolvendo o exercicio de NodeJs

--- exercicio-node/calc.js
+++ exercicio-node/calc.js
@@ -6,8 +6,10 @@ module.exports = {
       return first - last;
     } else if (operation === "multiplicacao") {
       return first * last;
-    } else {
+    } else if (operation === "divisao") {
       return first / last;
+    } else {
+      return "Operação invalida";
     }
   },
 };
